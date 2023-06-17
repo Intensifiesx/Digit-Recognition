765bc1d6823d85ab2f521feb8688fde18a38b182
Zain <hindizain12@yahoo.com>
2023-06-16 23:06:43 -0700

    ADDED DATASETS

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
+# %%
 from PIL import Image
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.linear_model import LogisticRegression
+import os
 
-# Load the image
-image = Image.open("image.png")
+# %%
+folder_path = "ImageDataset"  # Specify the path to your folder here
+files = os.listdir(folder_path)
+print(files)
 
-# Convert the image to grayscale
-image = image.convert("L")
 
-# Resize the image to 16x16 if necessary
-image = image.resize((16, 16))
+# %%
+def print_image():
+    # Print the resulting array
+    for row in array:
+        for cell in row:
+            if cell == 0:
+                print(cell, end=" ")
+            else:
+                print("\033[91m" + str(cell) + "\033[0m", end=" ")
+        print()
 
-# Convert each pixel to a binary value (1 or 0)
-pixels = []
-for y in range(16):
-    for x in range(16):
-        pixel = image.getpixel((x, y))
-        binary = 1 if pixel < 128 else 0
-        pixels.append(binary)
 
-# Reshape the list into a 16x16 array
-array = [pixels[i : i + 16] for i in range(0, 256, 16)]
+df = pd.DataFrame()
+# %%
+for file in files:
+    # Process each image file
+    file_path = os.path.join(folder_path, file)
+    print("Processing:", file_path)
+    # Add your code to process the image file here
 
-# Print the resulting array
-for row in array:
-    print(" ".join(map(str, row)))
+    # Load the image and convert it to grayscale
+    image = Image.open(file_path).convert("L").resize((16, 16))
+
+    # Convert each pixel to a binary value (1 or 0)
+    pixels = []
+    for y in range(16):
+        for x in range(16):
+            pixel = image.getpixel((x, y))
+            binary = 1 if pixel < 128 else 0
+            pixels.append(binary)
+
+    # Reshape the list into a 16x16 array
+    array = [pixels[i : i + 16] for i in range(0, 256, 16)]  # Python list
+    print_image()
+
+    # Make it NUMPY array
+    array = np.array(array).ravel().reshape(1, -1)
+    df = df.append(pd.DataFrame(array))
+
+    # %%
