186cdd87bf8fedb65b73fb696d437f73b258620f
Zain <hindizain12@yahoo.com>
2023-06-18 18:03:23 -0700

    Before adding threads

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
+#
+# Title: Number Recognition
+# Description: A program that uses a random forest classifier to recognize numbers
+# Authors: Zain Hindi, Alex Akoopie
+# Date: 2023-06-18
+#
+
 # %%
 # ======Libraries======
-from PIL import Image
+import os
+import threading
 import numpy as np
 import pandas as pd
-import sklearn
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from PIL import Image
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.tree import DecisionTreeClassifier
-import os
-import random
-import warnings
-
-warnings.filterwarnings("ignore")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 # %%
@@ -35,15 +33,12 @@ def get_image(path):  # Get Image Function
                 for j in range(2):
                     pixel = image.getpixel((x + j, y + i))
                     binary = 0 if pixel[3] == 0 else 1
-                    pixels[
-                        -1
-                    ] += binary  # Adds the number of pixels that are active in each chunk (0-4)
-    array = [
-        pixels[i : i + 14] for i in range(0, 196, 14)
-    ]  # Turns the pixels list into columns
-    return (
-        np.array(array).ravel().reshape(1, -1)
-    )  # Transforms the Python list into a NumPy array
+                    # Adds the number of pixels that are active in each chunk (0-4)
+                    pixels[-1] += binary
+    # Turns the pixels list into columns
+    array = [pixels[i : i + 14] for i in range(0, 196, 14)]
+    # Transforms the Python list into a NumPy array
+    return np.array(array).ravel().reshape(1, -1)
 
 
 # %%
@@ -51,22 +46,20 @@ def get_image(path):  # Get Image Function
 df = pd.DataFrame()
 label = pd.DataFrame()
 directory = "ImageDataset"
-for index, folder in enumerate(
-    os.listdir(directory)
-):  # Loops through the ImageDataset directory
+# Loops through the ImageDataset directory
+for index, folder in enumerate(os.listdir(directory)):
     subdirectory = os.path.join(directory, folder)
-    for subindex, file in enumerate(
-        os.listdir(subdirectory)
-    ):  # Loops through each folder in the ImageDataset directory
+    # Loops through each folder in the ImageDataset directory
+    for subindex, file in enumerate(os.listdir(subdirectory)):
         # Prematurely ends the loop to make loading faster
-        if subindex == 1000:
+        if subindex >= 1000:
             break
         file_path = os.path.join(subdirectory, file)
         array = get_image(file_path)  # Gets the image as an array of pixels
-        df = df.append(
-            pd.DataFrame(array)
-        )  # Adds the array to the dataframe as a new row
+        # Adds the array to the dataframe as a new row
+        df = df.append(pd.DataFrame(array))
         label = label.append(pd.DataFrame([index]))  # Keeps track of the label
+
 
 # %%
 # ======Initialization======
@@ -75,6 +68,8 @@ features = df.drop("Number", axis=1)  # Gets the features
 x_train, x_test, y_train, y_test = train_test_split(
     features, label, test_size=0.2, random_state=42
 )  # Splits the data into training and testing sets
+
+
 # %%
 # ======Testing======
 model = RandomForestClassifier(random_state=42)  # Defines the model
@@ -99,10 +94,10 @@ for file in os.listdir("Test"):
     predicted_probabilities = model.predict_proba(test)[0]
     predicted_number = model.predict(test)[0]
     print("Chance of it being a...")
-    for i in range(1, 10):
+    for i in range(1, 11):
         print(
-            f"{i}: {predicted_probabilities[i] * 100:05.2f}%",
-            end="\t" if i % 5 else "\n",
+            f"\033[92m{i-1}: {predicted_probabilities[i-1] * 100:05.2f}%",
+            end="\t" if i % 3 else "\n",
         )
     print(f"\033[37mHmmmm... I think the number is a {predicted_number}")
 # %%
