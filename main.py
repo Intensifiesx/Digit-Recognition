47f50c2cc376c1a1d66d626febb415e939eda441
Zain <hindizain12@yahoo.com>
2023-06-19 23:53:24 -0700

    multi

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-NUM_PROCESSES_DIRECTORIES = 10  # Number of processes for the directories
-IMAGES_TO_PROCESS = 2000  # Number of images to process
+NUM_PROCESSES_DIRECTORIES = 2  # Number of processes for the directories
+IMAGES_TO_PROCESS = 1000  # Number of images to process
 
 startTime = time.time()  # Gets the start time of the program
 
@@ -82,42 +82,42 @@ if __name__ == "__main__":
         f"Time to load n={IMAGES_TO_PROCESS} data ({NUM_PROCESSES_DIRECTORIES} Multi-Processes): {end_time - start_time:05.2f}s"
     )
 # %%
-# # ======Initialization======
-# df.insert(0, "Number", label)  # Appends the label to the first column of the dataframe
-# features = df.drop("Number", axis=1)  # Gets the features
-# x_train, x_test, y_train, y_test = train_test_split(
-#     features, label, test_size=0.2, random_state=42
-# )  # Splits the data into training and testing sets
+# ======Initialization======
+df.insert(0, "Number", label)  # Appends the label to the first column of the dataframe
+features = df.drop("Number", axis=1)  # Gets the features
+x_train, x_test, y_train, y_test = train_test_split(
+    features, label, test_size=0.2, random_state=42
+)  # Splits the data into training and testing sets
 
 
-# # %%
-# # ======Testing======
-# model = RandomForestClassifier(random_state=42)  # Defines the model
-# model.fit(x_train, y_train)  # Fits the parameters to the model
-# predictions = model.predict(x_test)  # Gets accuracy score from test set
-# print(f"Model Accuracy:\t{accuracy_score(y_test, predictions)}")
+# %%
+# ======Testing======
+model = RandomForestClassifier(random_state=42)  # Defines the model
+model.fit(x_train, y_train)  # Fits the parameters to the model
+predictions = model.predict(x_test)  # Gets accuracy score from test set
+print(f"Model Accuracy:\t{accuracy_score(y_test, predictions)}")
 
-# # ======Training======
-# for file in os.listdir("Test"):
-#     print("=" * 20)
-#     print(f"Testing {file}...")
+# ======Training======
+for file in os.listdir("Test"):
+    print("=" * 20)
+    print(f"Testing {file}...")
 
-#     # PATH TO TESTING IMAGE
-#     test_path = "Test/" + file
+    # PATH TO TESTING IMAGE
+    test_path = "Test/" + file
 
-#     # Gets testing image
-#     test = pd.DataFrame()
-#     array = get_image(test_path)
-#     test = test.append(pd.DataFrame(array))
+    # Gets testing image
+    test = pd.DataFrame()
+    array = getImage(test_path)
+    test = test.append(pd.DataFrame(array))
 
-#     # Gets and prints result
-#     predicted_probabilities = model.predict_proba(test)[0]
-#     predicted_number = model.predict(test)[0]
-#     print("Chance of it being a...")
-#     for i in range(1, 11):
-#         print(
-#             f"\033[92m{i-1}: {predicted_probabilities[i-1] * 100:05.2f}%",
-#             end="\t" if i % 3 else "\n",
-#         )
-#     print(f"\033[37mHmmmm... I think the number is a {predicted_number}")
-# # %%
+    # Gets and prints result
+    predicted_probabilities = model.predict_proba(test)[0]
+    predicted_number = model.predict(test)[0]
+    print("Chance of it being a...")
+    for i in range(1, 11):
+        print(
+            f"\033[92m{i-1}: {predicted_probabilities[i-1] * 100:05.2f}%",
+            end="\t" if i % 3 else "\n",
+        )
+    print(f"\033[37mHmmmm... I think the number is a {predicted_number}")
+# %%
